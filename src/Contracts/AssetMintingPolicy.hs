8af047a4c2bf892f09419dfc670c8035a6280675
Devin Holloway <devin.holloway@kinandcarta.com>
2023-04-01 11:43:01 -0600

    added pubkeyhash

--- src/Contracts/AssetMintingPolicy.hs
+++ src/Contracts/AssetMintingPolicy.hs
@@ -7,25 +7,32 @@ import           Data.ByteString.Lazy                 (toStrict)
 import           Data.ByteString.Short                (ShortByteString, toShort)
 import           Ledger.Typed.Scripts                 (mkUntypedMintingPolicy)
 import           Plutus.Script.Utils.V2.Typed.Scripts (MintingPolicy)
-import           Plutus.V2.Ledger.Api                 (Script, ScriptContext,
+import           Plutus.V2.Ledger.Api                 (PubKeyHash (..), Script,
+                                                       ScriptContext,
                                                        mkMintingPolicyScript,
                                                        unMintingPolicyScript)
 import qualified PlutusTx
-import           PlutusTx.Prelude                     hiding ((.))
+import           PlutusTx.Builtins.Class              (fromBuiltin, stringToBuiltinByteString)
+import           PlutusTx.Prelude                     hiding ((.), fromBuiltin)
 import           Prelude                              ((.))
 
-{-# INLINABLE mkPolicy #-}
-mkPolicy :: BuiltinData -> ScriptContext -> Bool
-mkPolicy _ _ = True
+publicKeyHash :: BuiltinByteString
+publicKeyHash = stringToBuiltinByteString "6f9f37db37e734116636c074243a4c35f9156da01e77c0ad63ab6574"
 
-policy :: MintingPolicy
-policy = mkMintingPolicyScript $
+{-# INLINABLE mkPolicy #-}
+mkPolicy :: PubKeyHash -> () -> ScriptContext -> Bool
+mkPolicy pkh () _ = True
+
+policy :: PubKeyHash -> MintingPolicy
+policy pkh = mkMintingPolicyScript $
     $$(PlutusTx.compile [|| wrap ||])
+    `PlutusTx.applyCode` PlutusTx.liftCode pkh
   where
-    wrap = mkUntypedMintingPolicy mkPolicy
+    wrap pkh' = mkUntypedMintingPolicy $ mkPolicy pkh'
 
 script :: Script
-script = unMintingPolicyScript policy
+script = unMintingPolicyScript
+       $ policy (PubKeyHash $ fromBuiltin publicKeyHash)
 
 scriptSBS :: ShortByteString
 scriptSBS = toShort . toStrict $ serialise script
